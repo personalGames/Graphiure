08c840d7c2fe6a4cadc346cedc4398cb0bc8fb57
Dorian Hawkmoon <dorian.cadenas@gmail.com>
2015-02-12 16:29:12 +0100

    Added tinyxml2 with test and passing everything

---
+++ XMLParser.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   XMLParser.cpp
+ * Author: dorian
+ * 
+ * Created on 12 de febrero de 2015, 16:11
+ */
+
+#include "XMLParser.h"
+
+XMLParser::XMLParser() {
+}
+
+XMLParser::XMLParser(const XMLParser& orig) {
+}
+
+XMLParser::~XMLParser() {
+}
+
---
+++ XMLParser.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   XMLParser.h
+ * Author: dorian
+ *
+ * Created on 12 de febrero de 2015, 16:11
+ */
+
+#ifndef XMLPARSER_H
+#define	XMLPARSER_H
+
+class XMLParser {
+public:
+    XMLParser();
+    XMLParser(const XMLParser& orig);
+    virtual ~XMLParser();
+private:
+
+};
+
+#endif	/* XMLPARSER_H */
+
