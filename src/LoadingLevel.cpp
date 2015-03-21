c33b7498ce7d14c460bb47a571a66406466e5e4e
Dorian Hawkmoon <dorian.cadenas@gmail.com>
2015-03-21 19:52:41 +0100

    Load all colisionable in map.

--- src/LoadingLevel.cpp
+++ src/LoadingLevel.cpp
@@ -11,9 +11,9 @@
 #include "IXMLParser.h"
 #include "StateMachineAnimation.h"
 
-LoadingLevel::LoadingLevel(Levels level, Level* levelToLoad, ResourceHolder<IDTextures,sf::Texture>* textures)
-: ParallelTask(), level(level), levelToLoad(levelToLoad){
-    this->textures=textures;
+LoadingLevel::LoadingLevel(Levels level, Level* levelToLoad, ResourceHolder<IDTextures, sf::Texture>* textures)
+: ParallelTask(), level(level), levelToLoad(levelToLoad) {
+    this->textures = textures;
 }
 
 LoadingLevel::~LoadingLevel() {
@@ -26,13 +26,13 @@ void LoadingLevel::runTask() {
         sf::Lock lock(mutex);
         completion = 5;
     }
-    
-    DataUnion data=DataUnion();
+
+    DataUnion data = DataUnion();
     //recojo el xml del nivel
-    IXMLParser* parser=IXMLParser::make_parser(TypeParser::MAP);
+    IXMLParser* parser = IXMLParser::make_parser(TypeParser::MAP);
     parser->setResources(textures);
     parser->setXML("Media/Levels/level1.xml");
-    
+
     //parseo el nivel
     parser->parse(data);
     StructMap* infoMap = data.map;
@@ -40,7 +40,7 @@ void LoadingLevel::runTask() {
         sf::Lock lock(mutex);
         completion = 10;
     }
-    
+
     //pass the information to build the scene with all this data readed
     levelToLoad->buildScene(infoMap);
     {
@@ -50,30 +50,30 @@ void LoadingLevel::runTask() {
 
     delete infoMap;
     delete parser;
-    
+
     //recojo los elementos necesarios y los comunico al manejador de recursos
-    Entity* character=new Entity();
-    
-    sf::Transformable* trans=new sf::Transformable();
-    trans->setPosition(0,0);
-    character->Add<sf::Transformable*>("Position",trans);
-    character->Add<sf::Vector2f>("Velocity",sf::Vector2f(0,0));
-    character->Add<float>("MaxVelocity",200.f);
-    
+    Entity* character = new Entity();
+
+    sf::Transformable* trans = new sf::Transformable();
+    trans->setPosition(0, 0);
+    character->Add<sf::Transformable*>("Position", trans);
+    character->Add<sf::Vector2f>("Velocity", sf::Vector2f(0, 0));
+    character->Add<float>("MaxVelocity", 200.f);
+
     levelToLoad->setCharacter(character);
-    
-    parser=IXMLParser::make_parser(TypeParser::STATE_MACHINE);
+
+    parser = IXMLParser::make_parser(TypeParser::STATE_MACHINE);
     parser->setResources(textures);
     parser->setXML("Media/Data/StateMachines.xml");
     parser->parse(data);
-    StateMachine* state=data.stateMachine;
+    StateMachine* state = data.stateMachine;
     delete parser;
-    
-    parser=IXMLParser::make_parser(TypeParser::ANIMATION);
+
+    parser = IXMLParser::make_parser(TypeParser::ANIMATION);
     parser->setResources(textures);
     parser->setXML("Media/Data/Animations.xml");
-    
-    StateMachineAnimation* machineAnimation=new StateMachineAnimation(*state);
+
+    StateMachineAnimation* machineAnimation = new StateMachineAnimation(*state);
     parser->parse(data);
     machineAnimation->setAnimations(data.animations->animations);
     character->Add<StateMachineAnimation*>("Drawable", machineAnimation);
@@ -82,14 +82,14 @@ void LoadingLevel::runTask() {
         sf::Lock lock(mutex);
         completion = 70;
     }
-    
-    
+
+
     //seteo las colisiones, de momento en debug
     //las seteo a mano directamente
-    SystemCollision* collision=new SystemCollision(levelToLoad->getBounds());
-    
+    SystemCollision* collision = new SystemCollision(levelToLoad->getBounds());
+
     //a mi personaje le doy la colisión de su cuerpo
-    Collision* coli=new Collision();
+    Collision* coli = new Collision();
     sf::VertexArray quad(sf::Quads, 4);
     quad[0].position = sf::Vector2f(0, 0);
     quad[1].position = sf::Vector2f(32, 0);
@@ -97,32 +97,34 @@ void LoadingLevel::runTask() {
     quad[3].position = sf::Vector2f(0, 32);
     coli->addArrayVertex(quad);
     character->Add<Collision*>("Collision", coli);
-    
-    
-    
+
+
+
     // y una colisión cualquiera para probar
-    Entity* arg=new Entity();
-    parser=IXMLParser::make_parser(TypeParser::COLLISIONS);
+    Entity* colisionable;
+    parser = IXMLParser::make_parser(TypeParser::COLLISIONS);
     parser->setXML("Media/Levels/level1.xml");
     parser->parse(data);
 
-    coli=data.collisions->at(0);
     //la posición hay que ajustarla al tamaño modificado del mapa
     //se saca el factor del mapa y se aplica
-    sf::Vector2f ratio=levelToLoad->getRatio();
-    coli->applyRatio(ratio);
-    arg->Add<sf::Transformable*>("Position",coli->getTransform());
-    
-    
-    arg->Add<Collision*>("Collision", coli);
-    
+    sf::Vector2f ratio = levelToLoad->getRatio();
+    for (std::vector<Collision*>::iterator it = data.collisions->begin();
+            it != data.collisions->end(); ++it) {
+        coli=*it;
+        coli->applyRatio(ratio);
+        colisionable=new Entity();
+        colisionable->Add<sf::Transformable*>("Position", coli->getTransform());
+        colisionable->Add<Collision*>("Collision", coli);
+        collision->addCollisionable(colisionable);
+    }
     //meto mi personaje
     collision->addCollisionable(character);
     //y al colisionable
-    collision->addCollisionable(arg);
     
+
     levelToLoad->setSystemCollision(collision);
-    
+
     { // finished may be accessed from multiple threads, protect it
         sf::Lock lock(mutex);
         completion = 100;
